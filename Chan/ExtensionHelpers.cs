3cb5f7339df0cc6aae6620a5f8dd9d61df11f474
Maartyl <kuronomaartyl@gmail.com>
2015-07-12 14:58:32 +0200

    format on strings

--- Chan/ExtensionHelpers.cs
+++ Chan/ExtensionHelpers.cs
@@ -5,15 +5,15 @@ namespace Chan
 {
   internal static class ExtensionHelpers {
     internal static string Format(this string s, object a) {
-      string.Format(s, a);
+      return string.Format(s, a);
     }
 
     internal static string Format(this string s, object a, object b) {
-      string.Format(s, a, b);
+      return string.Format(s, a, b);
     }
 
     internal static string Format(this string s, params object[] args) {
-      string.Format(s, args);
+      return string.Format(s, args);
     }
   }
 }
